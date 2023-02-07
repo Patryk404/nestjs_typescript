66522b6f6a45ac3b44a56e74eea51b43afa8c21b
Patryk <patrykkurek@student.agh.edu.pl>
2023-02-08 00:11:34 +0100

    types_typescript

---
+++ types_typescript/annotations/variables.ts
@@ -0,0 +1,26 @@
+const apples: number = 5; // type annotation
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null; 
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["hello","world"];
+let myNumbers: number[] = [1,2,3,4,5,6,7]; 
+let truths: boolean[] = [true,true,false];
+
+// Classes
+class Car{
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: {x: number; y:number} = {
+    x: 10,
+    y: 20
+};
