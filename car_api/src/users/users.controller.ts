18addea20c97193420b1c358fd54d8039bf1efe7
Patryk404 <patryk744@op.pl>
2023-02-18 11:30:08 +0100

    Added full unit testing

--- car_api/src/users/users.controller.ts
+++ car_api/src/users/users.controller.ts
@@ -9,18 +9,17 @@ import {
     Query,
     NotFoundException,
     Session,
-    UseInterceptors,
     UseGuards
   } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service'; 
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import {User} from "./user.entity";
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 // import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
   @Controller('auth')
   @Serialize(UserDto) // Serialize globally
@@ -69,8 +68,8 @@ import { AuthGuard } from 'src/guards/auth.guard';
     }
   
     @Get('/:id')
-    async findUser(@Param('id') id: string) {
-      const user = await this.usersService.findOne(parseInt(id));
+    async findUser(@Param('id') id: number) {
+      const user = await this.usersService.findOne(id);
       if (!user) {
         throw new NotFoundException('user not found');
       }
